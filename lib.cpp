484c37486d0fa5c8ad13152236b78093241d3b13
MartaAponte <128090552+MartaAponte@users.noreply.github.com>
2023-05-24 19:20:14 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
+#include "lib.h"
+float massimo  (int dim , float max[]){
+
+    float   m = max[0] ;
+
+    for (int j =0 ; j<dim; j ++ ){
+
+        if ( max [j]> m ){
+
+            m = max[j];
+
+        }
+    }
+    return m;
+}
 
