ef2ad654b4d32a03c79f467cd59ac3a29c178ddd
MartaAponte <128090552+MartaAponte@users.noreply.github.com>
2023-05-24 19:21:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int i ;
+    cin>>i;
+    float max[i];
+    for(int j = 0 ; j< i ;j ++ ){
+        cin>> max[j];
+
+
+    }
+     float m = massimo ( i , max ) ;
+
+
+    cout<<  m  ;
+
+
+    return 0 ;
 }
